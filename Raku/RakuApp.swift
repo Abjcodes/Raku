d2e12a313be5cd923c7b9298f5ccdc6ad645230d
Abjcodes <abijithmv01@gmail.com>
2025-04-03 23:47:22 +0530

    Initial Commit

---
+++ Raku/RakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RakuApp.swift
+//  Raku
+//
+//  Created by Abijith Vasanthakumar on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RakuTests/RakuTests.swift
@@ -0,0 +1,17 @@
+//
+//  RakuTests.swift
+//  RakuTests
+//
+//  Created by Abijith Vasanthakumar on 03/04/25.
+//
+
+import Testing
+@testable import Raku
+
+struct RakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
